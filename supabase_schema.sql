324df795665041701665d5fcaa4d61a3684bc256
Rodrigo Baquerizo <rvillacortabaquerizo@gmail.com>
2026-05-19 16:30:48 +0200

    docs: actualizar esquema de base de datos con columna Tournament Key

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -8,7 +8,7 @@
 DROP TABLE IF EXISTS public.atp_matches;
 CREATE TABLE public.atp_matches (
     "ID Partido" TEXT PRIMARY KEY,
-    "Torneo" TEXT, "Fecha" TEXT, "Superficie" TEXT,
+    "Torneo" TEXT, "Tournament Key" TEXT, "Fecha" TEXT, "Superficie" TEXT,
     "Jugador 1" TEXT, "J1 Key" TEXT, "J1 Puntos ATP" TEXT,
     "Jugador 2" TEXT, "J2 Key" TEXT, "J2 Puntos ATP" TEXT,
     "J1 H2H" TEXT, "J1 H2H %" TEXT, "J2 H2H" TEXT, "J2 H2H %" TEXT,
@@ -21,7 +21,7 @@ CREATE TABLE public.atp_matches (
 DROP TABLE IF EXISTS public.challenger_matches;
 CREATE TABLE public.challenger_matches (
     "ID Partido" TEXT PRIMARY KEY,
-    "Torneo" TEXT, "Fecha" TEXT, "Superficie" TEXT,
+    "Torneo" TEXT, "Tournament Key" TEXT, "Fecha" TEXT, "Superficie" TEXT,
     "Jugador 1" TEXT, "J1 Key" TEXT, "J1 Puntos ATP" TEXT,
     "Jugador 2" TEXT, "J2 Key" TEXT, "J2 Puntos ATP" TEXT,
     "J1 H2H" TEXT, "J1 H2H %" TEXT, "J2 H2H" TEXT, "J2 H2H %" TEXT,
